2b8e62a3b3cffe743e571605d0dd52be648fd10f
Shaoting Peng <sp4767@nyu.edu>
2022-09-25 22:01:35 -0400

    Fixed PyTorch Issue
    Fixed the issue that PyTorch takes up too much allocated mamory and crash the program. Run the fixNvPe.py to fix that.

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from Auto3D.auto3D import options, main
 
 if __name__ == "__main__":
     input_path = "example/files/smiles.smi"
-    args = options(input_path, k=1)   #args specify the parameters for Auto3D 
+    args = options(input_path, k=1, capacity=4)   #args specify the parameters for Auto3D 
     out = main(args)                  #main accepts the parameters and runs Auto3D
